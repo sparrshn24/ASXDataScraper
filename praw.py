43b4af36907531f80cca6deb3894266df0e33bfd
Sparrsh Nagdda(s3810177) <67099087+sparrshn24@users.noreply.github.com>
2023-06-17 13:23:31 +1000

    Added new scraping .py file

---
+++ praw.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install praw
+
+
+# In[7]:
+
+
+from praw.models import MoreComments
+import requests
+import pandas as pd
+import pytz
+import json
+import praw
+
+
+from datetime import *
+from dateutil import parser
+from comments import *
+
+reddit = praw.Reddit(
+    client_id="******",
+    client_secret="******",
+    password="*****",
+    user_agent="Scraping Aus Financial Subreddits.",
+    username="ThEhIGhGrEYmATTER",
+)
+
+api = PushshiftAPI(reddit)
+
+
+# In[8]:
+
+
+subreddit = reddit.subreddit("ASX")
+
+
+# In[9]:
+
+
+def getPostAsDict(post):
+    individual_post={}
+    individual_post['Subreddit'] = str(post.subreddit)
+    individual_post['URL'] = str(post.url)
+    individual_post['Subtext']= post.selftext
+    individual_post['Title']=post.title
+    individual_post['ID']=post.id
+#     individual_post['Kind']=post.kind
+    individual_post['Response']=getResponse(post)
+    individual_post['Upvote Ratio']=post.upvote_ratio
+    individual_post['Time']= datetime.fromtimestamp(post.created).strftime('%Y-%m-%dT%H:%M:%SZ')
+                    
+    return individual_post
+                
+
+
+# In[28]:
+
+
+def getResponse(post):
+    response=[]
+    submission = reddit.submission(post.id)
+    submission.comments.replace_more(limit=None)
+    flag=False
+    for comment in submission.comments.list():
+        response_str = str(comment.body)
+        blocks = response_str.split("\n")
+        for block in blocks:
+            if(block!=''):
+                response.append(block)
+    return response    
+
+
+# In[ ]:
+
+
+date = datetime.now(timezone.utc)
+req_date = utc_datetime = datetime(2018,7,31, 12, 0, 0, tzinfo = pytz.utc)
+while(date>req_date):
+    for submission in subreddit.new(limit=None):
+        post_dict = getPostAsDict(submission)
+        print(type(post_dict))
+        fullname = submission.fullname
+        date = datetime.strptime(post_dict['Time'],'%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.utc)
+        time = post_dict['Time']
+        path='ASX/'+time[0:13]
+        
+        print(post_dict)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
